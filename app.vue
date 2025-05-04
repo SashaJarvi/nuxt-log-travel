7159364c832e91c6c2ccd231fb899d7afb448f27
SashaJarvi <alexandrermakov2@gmail.com>
2025-05-04 23:00:37 +0400

    added the tailwind, daisyui and icons

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
+  <button class="btn btn-secondary">
+    View map <Icon name="tabler:map" />
+  </button>
 </template>
